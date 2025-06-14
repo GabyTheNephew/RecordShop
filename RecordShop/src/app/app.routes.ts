68030032f47901c9a54675dbedd690d1bb2aa260
GabyTheNephew <gabriel.slav@student.unitbv.ro>
2025-06-14 19:58:37 +0300

    Implemented UI for registering but without functionality

--- RecordShop/src/app/app.routes.ts
+++ RecordShop/src/app/app.routes.ts
@@ -1,8 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [{
-    path: 'home',
-    loadComponent: () => import('./features/landing-page/home-page/home-page.component')
-        .then(m => m.HomePageComponent),
-    data: { preload: true }
-},];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./features/login-page/login/login.component')
+            .then(m => m.LoginComponent),
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./features/register-page/register/register.component')
+            .then(m => m.RegisterComponent),
+    },
+    {
+
+        path: 'home',
+        loadComponent: () => import('./features/landing-page/home-page/home-page.component')
+            .then(m => m.HomePageComponent),
+    },
+    {
+        path: '**',
+        redirectTo: '/login'
+    }
+];
